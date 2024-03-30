43ce344a262a4c8330d2eaeafb871c5b764b87a5
as6325400 <105158172+as6325400@users.noreply.github.com>
2024-03-31 02:21:15 +0800

    change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5174/' # 允许的源（你的前端地址），可以使用 '*' 代表允许所有源
+    origins 'http://localhost:5174' # 允许的源（你的前端地址），可以使用 '*' 代表允许所有源
 
     resource '*', # 允许访问的资源路径，'*' 代表所有路径
       headers: :any, # 允许的头信息
