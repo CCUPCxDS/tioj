7dddee868fbc0b28e96bc185088541a0b08960f6
as6325400 <105158172+as6325400@users.noreply.github.com>
2024-03-31 01:05:45 +0800

    add cors

---
+++ config/initializers/cors.rb
@@ -0,0 +1,12 @@
+# config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*' # 允许的源（你的前端地址），可以使用 '*' 代表允许所有源
+
+    resource '*', # 允许访问的资源路径，'*' 代表所有路径
+      headers: :any, # 允许的头信息
+      methods: [:get, :post, :put, :patch, :delete, :options, :head], # 允许的 HTTP 方法
+      credentials: true # 是否允许发送 cookies（视你的需求而定）
+  end
+end
